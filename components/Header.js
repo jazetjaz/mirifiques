744bd2a7b77d621b62b577fc6661e5e9fcd4b0d4
Alami Erhaidi <alamierhaidi@iMac-de-Alami.local>
2021-09-07 20:04:02 +0200

    first commit

--- components/Header.js
+++ components/Header.js
@@ -7,19 +7,27 @@ function Header() {
      <header className="sticky top-0 z-50  bg-white  ">
 
 
-<div class="grid grid-cols-3 ">
+<div class="flex">
+    <div class="w-auto md:w-full border-b flex">
+    <FlagIcon className="h-5 w-5 text-yellow-800 ml-3"/>
+<div class=" ml-1 hidden md:block text-lg ">mirifiques</div>
 
-<div class="w-auto bg-red-700 sm:w-full ">
-    yyy
-  </div>
 
-  <div class=" bg-red-400 ">
-    yttt
-  </div>
 
-  <div class=" bg-yellow-400 ">
-    ghbn
-  </div>
+
+    </div>
+
+
+
+    <div class="w-auto md:w-full border-b">second Columncccccccccccccccccc</div>
+    
+    
+    
+    
+    <div class="w-auto md:w-full border-b">Third Column</div>
+
+
+
 </div>
 
 
