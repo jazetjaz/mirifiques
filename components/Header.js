7ed2248edd56ccc4360ebb1e8bc95a0b32e7a3f4
Alami Erhaidi <alamierhaidi@iMac-de-Alami.local>
2021-09-08 07:25:38 +0200

    first commit

--- components/Header.js
+++ components/Header.js
@@ -8,9 +8,9 @@ function Header() {
 
 
 <div class="flex">
-    <div class="w-auto md:w-full border-b flex">
-    <FlagIcon className="h-5 w-5 text-yellow-800 ml-3"/>
-<div class=" ml-1 hidden md:block text-lg ">mirifiques</div>
+    <div class="w-auto md:w-full  flex">
+    <FlagIcon className="h-5 w-5 ml-3"/>
+<div class=" ml-1 mr-1 tracking-wide hidden md:block text-lg ">mirifiques</div>
 
 
 
@@ -19,12 +19,21 @@ function Header() {
 
 
 
-    <div class="w-auto md:w-full border-b">second Columncccccccccccccccccc</div>
+    <div class="flex w-full  md:w-full   ">
+    <input className="pl-1 w-full   md:bg-transparent outline-none   placeholder-black" type="text" placeholder="Votre recherche ..." />
+    <SearchIcon className="h-5 w-5 "/>	 
+
+
+
+    </div>
     
     
     
     
-    <div class="w-auto md:w-full border-b">Third Column</div>
+    <div class="w-auto md:w-full  ml-1">
+    <div class="text-right md:absolute right-5">loginsignup</div>
+
+    </div>
 
 
 
